72c44157a77d575080b7c8c9d2119dc89e3c6e00
KellyKiiru <infowithkiiru@gmail.com>
2023-08-19 00:27:44 +0300

    add allauth.urls

--- auth_project/urls.py
+++ auth_project/urls.py
@@ -5,8 +5,9 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('logout', LogoutView.as_view()),
-    path('auth/',include('auth_app.urls'))
+    path('app/',include('auth_app.urls')),
 
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view())
 ]
