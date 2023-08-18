ffccd2ae7784e921241d6b101f35976d630832fc
KellyKiiru <infowithkiiru@gmail.com>
2023-08-19 00:07:54 +0300

    add authentication backends config

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
